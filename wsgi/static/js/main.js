ed8e688e7fffd71884f94c51ab2627de15078a71
Joe Bowen <joe.b.bowen@gmail.com>
2015-06-13 09:32:37 -0600

    Fix mysql

--- wsgi/static/js/main.js
+++ wsgi/static/js/main.js
@@ -1,25 +1,25 @@
-this.tooltip = function(){	
+this.tooltip1 = function(){	
 	/* CONFIG */		
 		xOffset = 10;
 		yOffset = 20;		
 		// these 2 variable determine popup's distance from the cursor
 		// you might want to adjust to get the right result		
 	/* END CONFIG */		
-	$("a.tooltip").hover(function(e){											  
+	$("a.tooltip1").hover(function(e){											  
 		this.t = this.title;
 		this.title = "";									  
 		$("body").append("<p id='tooltip'>"+ this.t +"</p>");
-		$("#tooltip")
+		$("#tooltip1")
 			.css("top",(e.pageY - xOffset) + "px")
 			.css("left",(e.pageX + yOffset) + "px")
 			.fadeIn("fast");		
     },
 	function(){
 		this.title = this.t;		
-		$("#tooltip").remove();
+		$("#tooltip1").remove();
     });	
-	$("a.tooltip").mousemove(function(e){
-		$("#tooltip")
+	$("a.tooltip1").mousemove(function(e){
+		$("#tooltip1")
 			.css("top",(e.pageY - xOffset) + "px")
 			.css("left",(e.pageX + yOffset) + "px");
 	});			
@@ -29,5 +29,5 @@ this.tooltip = function(){
 
 // starting the script on page load
 $(document).ready(function(){
-	tooltip();
+	tooltip1();
 });
