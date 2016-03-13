f3b791a1a002c130f4e7044f1b14c4a3f67fa34e
Joe Bowen <joe.b.bowen@gmail.com>
2016-03-13 13:49:35 -0600

    Updated dev settings

--- wsgi/BealsTreasure_Project/dev_settings.py
+++ wsgi/BealsTreasure_Project/dev_settings.py
@@ -35,7 +35,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request",)
 
-TEMPLATE_DIRS = ('/home/ubuntu/workspace/wsgi/templates/',)
+TEMPLATE_DIRS = ('../templates/',)
 
 ALLOWED_HOSTS = ['*']
 
